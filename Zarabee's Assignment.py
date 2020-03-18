3e71a1f0545e0cc40d1d8902983fb181829e7ef5
(Debbie) <(poffiamosdeborah@gmail.com)>
2020-03-18 04:29:00 -0700

    pyt.assignment

---
+++ Zarabee's Assignment.py
@@ -0,0 +1,43 @@
+x = input()
+y = int(x)**0.5
+print(f'value is {y}')
+
+x = input()
+y = int(x)**0.5
+print(f'value is {y}')
+
+x = input()
+y = int(x)**0.5
+print(f'value is {y}')
+
+x = input()
+y = int(x)**0.5
+print(f'value is {y}')
+
+x = input()
+y = int(x)**0.5
+print(f'value is {y}')
+
+x = input()
+y = int(x)**0.5
+print(f'value is {y}')
+
+x = input()
+y = int(x)**0.5
+print(f'value is {y}')
+
+x = input()
+y = int(x)**0.5
+print(f'value is {y}')
+
+x = input()
+y = int(x)**0.5
+print(f'value is {y}')
+
+x = input()
+y = int(x)**0.5
+print(f' value is {y}')
+
+x = input()
+y = int(x)**0.5
+print(f'value is {y}')
